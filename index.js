372bf7852d4589dcd48c9962edfae0a21b0373da
Leon Dail <leondailofficial@gmail.com>
2019-08-20 18:20:35 -0700

    finished numeric validation

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict';
 
+function pushData() {
+}
+
+function checkSubmissionData() {
+  let basedata = $('#base-meal-price').val();
+  let taxdata = $('#tax-rate').val();
+  let tipdata = $('#tip-percentage').val();
+  if ( $.isNumeric(basedata) && $.isNumeric(taxdata) && $.isNumeric(tipdata)) {
+    $('.display-error').text('');
+    return true;
+  }
+  $('.display-error').text('Please enter a valid number in the form');
+}
+
 function submitMealDetails() {
-  $('#submit-form').on('submit','.submit-data',e => {
+  $('#submit-form').submit(e => {
     e.preventDefault();
     console.log('form submitted');
+    let datacheck = checkSubmissionData();
+    if (datacheck) {
+      pushData();
+    }
+
   });
 }
 
