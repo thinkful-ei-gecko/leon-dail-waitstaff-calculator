a9e731850d8c596f0ba1f9f213d7278bc1a8dcb0
Leon Dail <leondailofficial@gmail.com>
2019-08-20 21:31:47 -0700

    reactified the code a bit more

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 'use strict';
 
-const database = {
-  tipTotal : 0,
-  mealCount : 0,
-  averageTip : 0
-};
+const database = {};
 
 function startPage() {
+  database.tipTotal = 0;
+  database.mealCount = 0;
+  database.averageTip = 0;
   loadEmptySubmitForm();
-  loadEmptyCustomerCharges();
-  loadEmptyEarningsInfo();
+  resetRightSide();
 }
 
 function loadEmptySubmitForm() {
   $('#submit-form').html(`
   <div class="submit-flex">
                     <div class="row">
-                        <label for="base-meal-price" class="">Base Meal Price: </label>
+                        <label for="base-meal-price">Base Meal Price: </label>
                         <span> $ <input type="text" class="long-input" id="base-meal-price"></span>
                     </div>
                     <div class="row">
-                        <label for="tax-rate" class="">Tax Rate:</label>
-                        <span>% <input type="text" class="short-input" id="tax-rate"></span>
+                        <label for="tax-rate">Tax Rate:</label>
+                        <span> % <input type="text" class="short-input" id="tax-rate"></span>
                     </div>
                     <div class="row">
-                        <label for="tip-percentage" class="">Tip Percentage:</label>
-                        <span>% <input type="text" class="short-input" id="tip-percentage"></span>
+                        <label for="tip-percentage">Tip Percentage:</label>
+                        <span> % <input type="text" class="short-input" id="tip-percentage"></span>
                     </div>
                 </div>
                 <div class="row">
@@ -35,28 +33,19 @@ function loadEmptySubmitForm() {
                 <div class="display-error"></div>`);
 }
 
-function loadEmptyCustomerCharges() {
+function resetRightSide() {
   $('#customer-charges-info').html(`
   <div class="charges-box">
-      <p id="subtotal">Subtotal <span id="subtotal-number">0</span></p>
-      <p id="tip">Tip <span id="tip-number">0</span></p>
-      <p id="total">Total <span id="total-number">0</span></p>
+      <p>Subtotal <span id="subtotal-number">0</span></p>
+      <p class="underline">Tip <span id="tip-number">0</span></p>
+      <p>Total <span id="total-number">0</span></p>
   </div>`);
-}
-
-function loadEmptyEarningsInfo() {
   $('#earnings-box-info').html(`
-  <p id="tip-total">Tip Total <span id="tip-total-number">0</span></p>
-  <p id="meal-count">Meal count <span id="meal-count-number">0</span></p>
-  <p id="average-tip">Average tip per meal <span id="average-tip-number">0</span></p>`);
+  <p>Tip Total <span id="tip-total-number">0</span></p>
+  <p>Meal count <span id="meal-count-number">0</span></p>
+  <p>Average tip per meal <span id="average-tip-number">0</span></p>`);
 }
-function addToEarningsInfo() {
-  let subtotal = parseInt($('#base-meal-price').val(),10);
-  let taxtotal = parseInt($('#tax-rate').val(),10);
-  let tiptotal = parseInt($('#tip-percentage').val(),10);
-  let subtotalNumber = subtotal + (subtotal * (taxtotal/100));
-  let tipNumber = subtotal * (tiptotal/100);
-
+function addToEarningsInfo(tipNumber) {
   database.tipTotal += tipNumber;
   database.mealCount++;
   database.averageTip = database.tipTotal / database.mealCount;
@@ -70,16 +59,11 @@ function addToCustomerCharges() {
   let taxtotal = parseInt($('#tax-rate').val(),10);
   let tiptotal = parseInt($('#tip-percentage').val(),10);
   let subtotalNumber = subtotal + (subtotal * (taxtotal/100));
-  let tipNumber = subtotal * (tiptotal/100);
+  let tipNumber = subtotal * (tiptotal/100); 
   $('#subtotal-number').text(subtotalNumber);
   $('#tip-number').text(tipNumber);
   $('#total-number').text(subtotalNumber + tipNumber);
-}
-
-function pushData() {
-  addToCustomerCharges();
-  addToEarningsInfo();
-
+  return tipNumber;
 }
 
 function checkSubmissionData() {
@@ -96,11 +80,10 @@ function checkSubmissionData() {
 function submitForm() {
   $('#submit-form').submit(e => {
     e.preventDefault();
-    let current = $(document.activeElement).attr('class');
-    if (current === 'submit-data') {
-      let datacheck = checkSubmissionData();
-      if (datacheck) {
-        pushData();
+    let currentClass = $(document.activeElement).attr('class');
+    if (currentClass === 'submit-data') {
+      if (checkSubmissionData()) {
+        addToEarningsInfo(addToCustomerCharges());
       }
     }
     else { loadEmptySubmitForm(); }
@@ -110,12 +93,7 @@ function submitForm() {
 function resetPage() {
   $('#reset-form').submit(e => {
     e.preventDefault();
-    database.tipTotal = 0;
-    database.mealCount = 0;
-    database.averageTip = 0;
-    loadEmptySubmitForm();
-    loadEmptyCustomerCharges();
-    loadEmptyEarningsInfo();
+    startPage();
   })
 }
 
